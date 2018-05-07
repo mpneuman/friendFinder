3d5b735948d83b7f6f645fae14dfac07f1dab283
Michael Neuman <mpneuman@Michaels-MacBook-Pro.local>
2018-05-07 15:11:32 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+
+var friends = [{
+    "name": "Alex",
+    "photo": "https://pbs.twimg.com/profile_images/798632247762948096/ycGYVWvr_400x400.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Jean-luc",
+    "photo": "https://img.cinemablend.com/filter:scale/cb/e/3/e/6/4/2/e3e642ac3be4f180b7195be0ad15645a2bcd2af4f9c65faef62178a57e899f41.jpg?mw=600",
+    "scores": [
+        3,
+        5,
+        5,
+        5,
+        3,
+        3,
+        1,
+        3,
+        5,
+        3
+    ]
+},
+{
+    "name": "Darth Vader",
+    "photo": "https://cdn.thisiswhyimbroke.com/images/darth-vader-figure.jpg",
+    "scores": [
+        5,
+        1,
+        5,
+        1,
+        3,
+        1,
+        5,
+        5,
+        5,
+        1
+    ]
+},
+{
+    "name": "Frodo Baggins",
+    "photo": "https://i.pinimg.com/736x/ba/56/24/ba56242e916086cc80b97922b0955ddd--frodo-baggins-the-lord.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5,
+        1,
+        5
+    ]
+},
+{
+    "name": "Tyrion Lannister",
+    "photo": "http://awoiaf.westeros.org/images/7/78/Tyrion_as_Hand.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1,
+        5,
+        1
+    ]
+}];
+
+
+module.exports = friends;
